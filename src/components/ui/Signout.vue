80e2df5660d0fc418ff88778b3cceda84d5d5544
0n10n7 <mikael.kokeritz@gmail.com>
2024-04-30 09:53:52 +0200

    added signout function to app page

---
+++ src/components/ui/Signout.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+import { useAuthStore } from '@/stores/store'
+
+const Auth = useAuthStore();
+Auth.Inject();
+console.log(Auth.auth.currentUser);
+</script>
+
+<template>
+    <h1 v-if="Auth.auth.currentUser"> User: {{ Auth.auth.currentUser.displayName }}</h1>
+</template>
+<style scoped></style>
