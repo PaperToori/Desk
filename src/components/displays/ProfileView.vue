149d01290207705577b6da52b30bc5e37a886ca0
0n10n7 <mikael.kokeritz@gmail.com>
2024-05-07 10:43:20 +0200

    started work on profile view

--- src/components/displays/ProfileView.vue
+++ src/components/displays/ProfileView.vue
@@ -2,9 +2,13 @@
 import {ref} from 'vue';
 
 let name = ref();
+let socialSecurityNumber = ref();
+let email = ref();
+let phoneNumber = ref();
+
 
 async function submitForm() {
-
+    //fetch
 }
 
 </script>
@@ -14,6 +18,27 @@ async function submitForm() {
 <form method="POST" @submit:prevent="submitForm">
     <label>
         Namn: <input type="text" v-model="name">
+    </label>
+    <label>
+        person nummer: <input type="text" Placeholder="YYYYMMDD-XXXX" v-model="socialSecurityNumber">
+    </label>
+    <label>
+        email: <input type="text" v-model="email">
+    </label>
+    <label>
+        telefon nummer: <input type="text" v-model="phoneNumber">
+    </label>
+    <label>
+        klass: <input type="" v-model="name" c>
+    </label>
+    <label>
+        adress: <input type="text" v-model="name">
+    </label>
+    <label>
+        poskod: <input type="text" v-model="name">
+    </label>
+    <label>
+        Vårdnashavare: <input type="text" v-model="name">
     </label>
     <button type="submit">Spara ändringar</button>
 </form>
