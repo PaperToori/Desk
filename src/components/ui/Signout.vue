16820c196c7665f23e45488277725496a58a4a57
0n10n7 <mikael.kokeritz@gmail.com>
2024-05-08 16:09:49 +0200

    ProfileView works if the user is a student

--- src/components/ui/Signout.vue
+++ src/components/ui/Signout.vue
@@ -1,13 +1,38 @@
 <script setup>
+import { signOut } from 'firebase/auth';
+import { useAuthStore } from '@/stores/store';
+import { ref, watch } from 'vue';
 
-import { useAuthStore } from '@/stores/store'
-
+let profile = ref();
 const Auth = useAuthStore();
 Auth.Inject();
-console.log(Auth.auth.currentUser);
+profile.value=Auth.auth.currentUser;
+console.log(profile);
+async function signout() {
+    console.log("attempting to signout");
+    signOut(Auth.auth)
+        .then(() => {
+            // Sign-out successful.
+            console.log('logged out');
+            window.location.reload();
+        })
+        .catch((error) => {
+            console.log("an error occured");
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.error(errorCode);
+            console.error(errorMessage);
+        });
+}
 </script>
 
 <template>
-    <h1 v-if="Auth.auth.currentUser"> User: {{ Auth.auth.currentUser.displayName }}</h1>
+
+    <div v-if="profile">
+        <a href="/#/profile">{{ profile.displayName }}</a>
+
+        <button @click="signout">Logga Ut</button>
+    </div>
+    <a href="/#/profile" v-else>logga in</a>
 </template>
 <style scoped></style>
