9f350af51822d5d1e4e744a3862893ab2a80e219
PaperToori <garratt.karl@elev.ga.ntig.se>
2024-05-14 16:22:58 +0200

    Displays info, nothing else

--- src/components/ui/GroupMembers.vue
+++ src/components/ui/GroupMembers.vue
@@ -1,19 +1,16 @@
 <script setup>
+
 const props = defineProps({ newMembers: Array, students: Array });
+
 </script>
 <template>
-    List of students goes here
-    <!-- <div v-for="member in props.newMembers">
-        <span>{{ member }}</span>
-        <button>Edit</button>
-        <button>Remove</button>
+    <div v-for="member in props.newMembers" class="items">
+        <span>{{ member.name }}</span>
+        <button @click="$emit('UpdateNewMembers', false, member)">Remove</button>
     </div>
-    <select v-model="selectedStudent">
+    <!-- <select v-model="selectedStudent">
         <option v-for="student in props.students" :value="student._id">{{ student.name }}</option>
     </select> -->
-
-
-
 
 
 
@@ -29,4 +26,14 @@ const props = defineProps({ newMembers: Array, students: Array });
     </div>
 </div> -->
 </template>
-<style scoped></style>
+<style scoped>
+.items {
+    display: flex;
+    justify-content: space-between;
+    width: 80%;
+    margin: 5px;
+    padding: 5px;
+    border: 1px solid black;
+    background-color: rgb(210, 210, 210);
+}
+</style>
